60c94bfbd6e74833bc222ed727d60d5f893f431d
JH.H <74130565+JIHEON-HYUN@users.noreply.github.com>
2025-04-03 17:51:23 +0900

    Update AutoSolver.h

--- AutoSolver.h
+++ AutoSolver.h
@@ -13,8 +13,13 @@ private:
 public:
 	AutoSolver();
 	~AutoSolver();
+
 	vector<Drawing*> Solution(Drawing* drawing);
+
+	// 노노그램은 반드시 해답이 한 개로 보장되지 않음
+	// 입력한 그림의 숫자 힌트가 나왔을 때, 역산하여 해답이 한 개인지 찾는 메소드
 	bool CheckUniqueSolution(Drawing* drawing);
+
 	void GetHint(Drawing* drawing, PlayScene* playScene);
 };
 
